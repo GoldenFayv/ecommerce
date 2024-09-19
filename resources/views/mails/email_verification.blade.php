89d95eab449411200b12ef351c1900b5c0b74cd2
GoldenFayv <favour.e2002@icloud.com>
2024-09-19 03:14:55 +0100

    Email Update

--- resources/views/mails/email_verification.blade.php
+++ resources/views/mails/email_verification.blade.php
@@ -10,7 +10,7 @@
 @section('content')
     <p>Hi {{ $data['name'] }},</p>
     <p>
-        We're excited to have you in the NKM Family! Your account has been successfully created.
+        We're excited to have you in the Brand Name Family! Your account has been successfully created.
     </p>
     <p>Please use the code below to verify your email address:</p>
     <h2 style="color: #6576ff;">{{ $data['code'] }}</h2>
