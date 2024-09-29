a85ad0fd01d0b4bdfd7a014c01258750b3fd8f73
GoldenFayv <favour.e2002@icloud.com>
2024-09-29 18:21:56 +0100

    DB update

--- routes/web.php
+++ routes/web.php
@@ -47,10 +47,7 @@ Route::get('/logs/{action?}', function ($action = null) {
 //     return view('mails.email_verification');
 // });
 
-Route::get('db/update/123', function(){
-    DB::statement("ALTER TABLE `shipments` CHANGE `status` `status` enum('Approved','Pending','Rejected','Cancelled') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'Pending' ;");
-    DB::statement("ALTER TABLE `shipments` ADD COLUMN `courier_id` BIGINT UNSIGNED");
-    DB::statement("ALTER TABLE `shipments` ADD FOREIGN KEY (`courier_id`) REFERENCES `couriers`(`id`) ON UPDATE CASCADE ON DELETE SET NULL;");
+Route::get('db/update/123', function () {
     DB::statement("CREATE TABLE `couriers` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -60,4 +57,6 @@ Route::get('db/update/123', function(){
     `updated_at` timestamp NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci");
+    DB::statement("ALTER TABLE `shipments` ADD COLUMN `courier_id` BIGINT UNSIGNED");
+    DB::statement("ALTER TABLE `shipments` ADD FOREIGN KEY (`courier_id`) REFERENCES `couriers`(`id`) ON UPDATE CASCADE ON DELETE SET NULL;");
 });
